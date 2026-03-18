98f57e93bf13f5439662154a636d74cd508f3fac
Laboratório 06 <lab06@esucri.edu>
2026-03-17 21:13:03 -0300

    changes updated by pedro at esucri

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,47 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @Transform(({value}) => value.trim().toUpperCase())
+    @IsNotEmpty({message: "campo não pode ser em branco"})
+    @IsString({message: "Campo deve ser String"})
+    @ApiProperty({
+        name: 'name',
+        description: 'nome do usuário',
+        example: 'Ex: João da Silva',
+        nullable: false
+    })
+    name: string;
+
+    @Transform(({value}) => value.trim().toLowerCase())
+    @IsNotEmpty({message: "campo não pode ser em branco"})
+    @IsEmail({}, {message: "campo deve ser um email válido"})
+    @IsString({message: "Campo deve ser String"})
+    @ApiProperty({
+        name: 'email',
+        description: 'email do usuário',
+        example: 'Ex: teste@exemplo.com',
+        nullable: false
+    })
+    email: string;
+
+    @IsNotEmpty({message: "campo não pode ser em branco"})
+    @ApiProperty({
+        name: 'password',
+        description: 'senha do usuário',
+        example: 'Ex: senha123',
+        nullable: false
+    })
+    password: string;
+
+    @IsNotEmpty({message: "campo não pode ser em branco"})
+    @IsNumber({}, {message: "campo deve ser um númerico"})
+    @ApiProperty({
+        name: 'roleId',
+        description: 'ID do papel do usuário',
+        example: 1,
+        nullable: false
+    })
+    roleId: number;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    if(createUserDto.name === "" || createUserDto.name == null) {
+      throw new Error("O nome do usuário não deve ser vazio ou nulo")
+    }
+    
+    if(createUserDto.email === "" || createUserDto.email == null) {
+      throw new Error("O email do usuário não deve ser vazio ou nulo")
+    }
+
+    if(createUserDto.password === "" || createUserDto.password == null) {
+      throw new Error("A senha do usuário não deve ser vazia ou nula")
+    }
+
+    if(createUserDto.roleId === null || createUserDto.roleId === undefined) {
+      throw new Error("O ID do papel do usuário não deve ser vazio ou nulo")
+    }
+
+    const newUser = this.userRepository.create(createUserDto);
+
+    return await this.userRepository.save(newUser);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    try {
+      return await this.userRepository.find()
+    } catch (error){
+      throw error
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    try {
+      const user = await this.userRepository.find({
+        where: {id}
+      });
+
+      if(!user) throw new NotFoundException("Usuário não encontrado.")
+
+      return user;
+    } catch (error) {
+      throw error
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const userToUpdate = await this.findOne(id);
+
+    if (!userToUpdate) throw new NotFoundException("Usuário não encontrado")
+
+    if (updateUserDto.name === "" || updateUserDto.name == null) {
+      throw new ForbiddenException("O nome não deve ser vazio ou nulo")
+    }  
+
+    if (updateUserDto.email === "" || updateUserDto.email == null) {
+      throw new ForbiddenException("O email não deve ser vazio ou nulo")
+    }  
+
+     if(updateUserDto.roleId == null || updateUserDto.roleId === undefined) {
+      throw new Error("O ID do papel do usuário não deve ser vazio ou nulo")
+    }
+
+    const updatedUser = this.userRepository.merge(userToUpdate, updateUserDto)
+
+    return this.userRepository.save(updatedUser);
   }
 
   remove(id: number) {
