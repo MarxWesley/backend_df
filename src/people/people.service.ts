721de64f9c1f9cfca9b70882f554a215dc974626
Wesley Marques <marques_bertin@hotmail.com>
2026-04-03 22:00:20 -0300

    feat: person

--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -1,34 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, Search } from '@nestjs/common';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Person } from './entities/person.entity';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class PeopleService {
   constructor(
     @InjectRepository(Person)
     private personRepository: Repository<Person>,
-  ) {}
-  
-  create(createPersonDto: CreatePersonDto) {
-    return 'This action adds a new person';
+  ) { }
+
+  async create(createPersonDto: CreatePersonDto) {
+    const cpfExists = await this.personRepository.findOne({ where: { cpf: createPersonDto.cpf } });
+
+    if (cpfExists) {
+      throw new ConflictException('CPF já existe');
+    }
+
+    const person = this.personRepository.create(createPersonDto);
+
+    return this.personRepository.save(person);
   }
 
-  findAll() {
-    return `This action returns all people`;
+  async findAll() {
+    const people = await this.personRepository.find({where: {ativo: true}});
+
+    if (!people) {
+      throw new NotFoundException('Nenhuma pessoa encontrada');
+    }
+
+    return people;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} person`;
+  async findOne(id: number) {
+    const person = await this.personRepository.findOne({ where: { id } });
+
+    if (!person) {
+      throw new NotFoundException('Pessoa não encontrada');
+    }
+
+    return person;
   }
 
-  update(id: number, updatePersonDto: UpdatePersonDto) {
-    return `This action updates a #${id} person`;
+  async findByNameOrCpf(serach: string) {
+    if(!Search) {
+      throw new NotFoundException('Parâmetro de busca é obrigatório');
+    }
+
+    return this.personRepository.createQueryBuilder('person')
+      .where('person.nome LIKE :search', { search: `%${serach}%` })
+      .orWhere('person.cpf LIKE :search', { search: `%${serach}%` })
+      .andWhere('person.ativo = :ativo', { ativo: true })
+      .orderBy('person.nome', 'ASC')
+      .getMany();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} person`;
+  async update(id: number, updatePersonDto: UpdatePersonDto) {
+    const person = await this.findOne(id);
+
+    if (updatePersonDto.cpf && updatePersonDto.cpf !== person.cpf) {
+      const cpfExists = await this.personRepository.findOne({ where: { cpf: updatePersonDto.cpf } });
+
+      if (cpfExists) {
+        throw new ConflictException('CPF já existe');
+      }
+    }
+
+    const updatedPerson = this.personRepository.merge(person, updatePersonDto);
+
+    return this.personRepository.save(updatedPerson);
+  }
+
+  async alterarStatus(id: number) {
+    const person = await this.findOne(id);
+
+    person.ativo = !person.ativo;
+
+    return await this.personRepository.save(person);
   }
 }
